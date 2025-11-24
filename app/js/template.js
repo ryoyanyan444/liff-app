d77b99d79e70d223bbcabae2d239589993f76516
Ryoya Wakabayashi <ryoyanyan444@gmail.com>
2025-11-24 16:41:06 +0900

    Update LIFF ID for template feature

--- app/js/template.js
+++ app/js/template.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 const CONFIG = {
-  LIFF_ID: '2008551240-vWN36gzR',  // ⭐ 既存のLIFF IDを一時的に使用
+  LIFF_ID: '2008551240-W6log3Gr',  // ⭐ 既存のLIFF IDを一時的に使用
   GAS_API_URL: 'https://script.google.com/macros/s/AKfycbwDM80XlwPf9A2oj1OnqEK7vKwy2ry-3rfIAUrV1Ri3WyEasa1pbnaUcBCk7UCVQQ-Y/exec'  // ⭐ あなたのGAS URL
 };
 
